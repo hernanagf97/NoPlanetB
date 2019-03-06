b04bbb95747afd53e86da9bca5110970349d3845
hernan <hernanagf97@gmail.com>
2019-03-06 17:44:46 +0100

    Primer commit

---
+++ app/src/main/java/com/example/noplanetb/BSplash.java
@@ -0,0 +1,44 @@
+package com.example.noplanetb;
+
+import android.os.Build;
+import android.support.v4.content.res.ResourcesCompat;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import com.bumptech.glide.Glide;
+import com.bumptech.glide.load.engine.DiskCacheStrategy;
+import com.bumptech.glide.request.RequestOptions;
+
+import java.io.File;
+
+public class BSplash extends AppCompatActivity {
+    ImageView imglogo;
+    LinearLayout lyout;
+    TextView txtlogo, txtloading;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.splashscreen);
+         imglogo =  (ImageView) findViewById(R.id.logo);
+         lyout = findViewById(R.id.layout);
+         txtlogo = findViewById(R.id.Titulo);
+         txtloading = findViewById(R.id.subTitulo);
+        setContentView(R.layout.splashscreen);
+       getSupportActionBar().hide();
+       Glide.with(this)
+                .load(R.drawable.rocketlogo)
+                .into(imglogo);
+            //Glide.with(this).load(R.drawable.rocketlogo).apply(RequestOptions.circleCropTransform()).into(imglogo);
+
+        Animation anim = AnimationUtils.loadAnimation(this, R.anim.fadein);
+        txtlogo.startAnimation(anim);
+
+    }
+}
